d1595428ba110ba799316ce5d5cd9607204a2179
verasha <davendra.shayna@ui.ac.id>
2023-10-13 00:15:57 +0700

    Update

---
+++ allanself.py
@@ -0,0 +1,44 @@
+import numpy as np
+def allanoadev_opt(y,t,rate):
+        
+    # === Parameters ===
+    
+    N = len(y)
+    # m array where 3 < m < N/3 
+    m = np.arange(3,1+round(N//3),1)
+    # Take tau0 as 1/rate
+    tau0 = 1/rate
+    # steps
+    step = 1
+    
+    # Empty variance array
+    allanvar = []
+    # Empty tau values array 
+    tau = []
+
+    # Iterate over values of m
+    for k in range(len(m)): 
+        kernels = np.ones(m[k])/m[k]
+        ybar = np.convolve(y,kernels,'valid')
+
+        # M is the number of times we can move the averaging window from the beginning to the end
+        M = len(ybar)
+
+        # Calculate allan var 
+        outersum = []
+        for j in np.arange(0,M-2*m[k]+1,step):
+            innersum = []
+            for i in np.arange(j,j+m[k]-1,step):
+                innersum.append(ybar[i+m[k]]-ybar[i])
+            outersum.append(0.5*(np.sum(innersum)/m[k])**2)
+
+        # Calculate allan variance
+        allanvar.append(np.sum(outersum)/(M-2*m[k]+1))
+        
+        # Calculate tau
+        tau.append(tau0*m[k])
+
+    # Calculate allan std dev
+    allanstd = np.sqrt(allanvar)
+        
+    return tau, allanstd
